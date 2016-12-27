37c84dd579782f058058d91cc0fabf1147ac187a
Sonal Garg <sgarg24@sapient.com>
2016-12-27 12:55:38 +0530

    Green: Added logic to fix test case for two unique words

--- src/main/java/com/practise/tdd/WordCount.java
+++ src/main/java/com/practise/tdd/WordCount.java
@@ -1,9 +1,20 @@
 package com.practise.tdd;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class WordCount {
 
-	public String getUnqiueWordsFromString(String input) {
-		return input + " 1";
+	public String getUnqiueWordsWithCountFromString(String input) {
+		String output = "";
+		
+		
+		List<String> uniqueWords = Arrays.asList(input.split(" "));		
+		for(String word : uniqueWords) {
+			output += word + " 1, ";
+		}
+		
+		return output.substring(0, output.length()-2);
 	}	
 
 }
