eecdde5311315de7d462578d0c9c2e3f9a242382
Sonal Garg <sgarg24@sapient.com>
2016-12-27 13:36:11 +0530

    Refactor: removed hardcoded count and introduced map to increment word counters

--- src/main/java/com/practise/tdd/WordCount.java
+++ src/main/java/com/practise/tdd/WordCount.java
@@ -1,20 +1,40 @@
 package com.practise.tdd;
 
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class WordCount {
 
 	public String getUnqiueWordsWithCountFromString(String input) {
 		String output = "";
 		
+		List<String> allWords = Arrays.asList(input.split(" "));
 		
-		List<String> uniqueWords = Arrays.asList(input.split(" "));		
-		for(String word : uniqueWords) {
-			output += word + " 1, ";
+		Map<String, Integer> wordCountMap = countUniqueWords(allWords);
+		for(Entry<String, Integer> entry : wordCountMap.entrySet()) {
+			output += entry.getKey() + " " + entry.getValue() + ", ";
 		}
 		
 		return output.substring(0, output.length()-2);
-	}	
+	}
+	
+	private Map<String, Integer> countUniqueWords(List<String> words) {
+		Map<String, Integer> uniqueWordCountMap = new HashMap<>();
+		words.forEach(word -> {
+			if(uniqueWordCountMap.containsKey(word)) {
+				Integer existingCount = uniqueWordCountMap.get(word);
+				uniqueWordCountMap.put(word, ++existingCount);
+			} else {
+				uniqueWordCountMap.put(word, 1);
+			}
+			
+		});
+		
+		return uniqueWordCountMap;
+	}
 
 }
