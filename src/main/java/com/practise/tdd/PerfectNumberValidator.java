74e5d6bebbea8a2487a62ab69d48fd91a02eb792
Sonal Garg <sgarg24@sapient.com>
2016-12-27 15:54:17 +0530

    Green: Added logic to calculate perfect number divisors

--- src/main/java/com/practise/tdd/PerfectNumberValidator.java
+++ src/main/java/com/practise/tdd/PerfectNumberValidator.java
@@ -1,10 +1,48 @@
 package com.practise.tdd;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class PerfectNumberValidator {
 
 	public boolean isPerfectNumber(int number) {
+		List<Integer> divisorsOfNumber = getAllDivisors(number);
+		
+		if(number!= 1 && sumOfDivisorsEqualsNumber(divisorsOfNumber, number))
+			return true;
+		
 		return false;
 	}
 	
+	private List<Integer> getAllDivisors(int dividend) {
+		List<Integer> validDivisors = new ArrayList<>();
+		validDivisors.add(new Integer(1));
+		
+		int remainder = dividend;
+		while(remainder > 1) {
+			int[] primeNumberDivisors = {2,3,5,7};
+			
+			for(int divisorIndex = 0; divisorIndex < primeNumberDivisors.length; divisorIndex++) {
+				if(remainder % primeNumberDivisors[divisorIndex] == 0) {
+					remainder = remainder/primeNumberDivisors[divisorIndex];
+					validDivisors.add(primeNumberDivisors[divisorIndex]);
+				}
+			}
+		}
+		
+		return validDivisors;
+	}
+	
+	
+	private boolean sumOfDivisorsEqualsNumber(List<Integer> divisorsOfNumber, int inputNumber) {
+		int sumOfdivisors = 0;
+		for(Integer divisor : divisorsOfNumber) {
+			sumOfdivisors += divisor;
+		}
+		
+		if(sumOfdivisors == inputNumber)
+			return true;
+		return false;
+	}
+
 }
