26b0395bf3e7f7ff014997fca24e26401f75a097
Sonal Garg <sgarg24@sapient.com>
2016-12-27 12:07:38 +0530

    Red: test code does not compile as main class is missing

---
+++ src/test/java/com/practise/tdd/WordCountShould.java
@@ -0,0 +1,19 @@
+package com.practise.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordCountShould {
+
+	WordCount wordCount;
+	
+	@Before
+	public void setup() {
+		wordCount = new WordCount();
+	}
+	
+	@Test
+	public void count_unique_words_in_single_word_string() {
+		
+	}
+}
