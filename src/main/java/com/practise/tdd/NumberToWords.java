2f6e603644cf08a1f7b299d2dde2f0581f4b4c8d
Sonal Garg <sgarg24@sapient.com>
2016-12-29 15:54:01 +0530

    Green: added logic to get text for zeros till teens

--- src/main/java/com/practise/tdd/NumberToWords.java
+++ src/main/java/com/practise/tdd/NumberToWords.java
@@ -1,14 +1,46 @@
 package com.practise.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberToWords {
 
+private Map<Integer, String> ones_and_teens = new HashMap<>();
+	
+	NumberToWords() {
+		initializeAllMaps();
+	}
+	
+	
 	public String getTexualRepresentationForNumber(Integer number) {
 		
 		return getTextForSingleDigitAndTeens(number);
 	}
 
 	private String getTextForSingleDigitAndTeens(Integer number) {
-		return String.valueOf(number);
+		return ones_and_teens.get(number);
 	}
 	
+	private void initializeAllMaps() {
+		ones_and_teens.put(new Integer(0), "zero");
+		ones_and_teens.put(new Integer(1), "one");
+		ones_and_teens.put(new Integer(2), "two");
+		ones_and_teens.put(new Integer(3), "three");
+		ones_and_teens.put(new Integer(4), "four");
+		ones_and_teens.put(new Integer(5), "five");
+		ones_and_teens.put(new Integer(6), "six");
+		ones_and_teens.put(new Integer(7), "seven");
+		ones_and_teens.put(new Integer(8), "eight");
+		ones_and_teens.put(new Integer(9), "nine");
+		ones_and_teens.put(new Integer(10), "ten");
+		ones_and_teens.put(new Integer(11), "eleven");
+		ones_and_teens.put(new Integer(12), "twelve");
+		ones_and_teens.put(new Integer(13), "thirteen");
+		ones_and_teens.put(new Integer(14), "fourteen");
+		ones_and_teens.put(new Integer(15), "fifteen");
+		ones_and_teens.put(new Integer(16), "sixteen");
+		ones_and_teens.put(new Integer(17), "seventeen");
+		ones_and_teens.put(new Integer(18), "eighteen");
+		ones_and_teens.put(new Integer(19), "nineteen");
+	}
 }
