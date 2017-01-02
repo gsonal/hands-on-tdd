52964b7adacda244b103b4979f8e18d007d4eac5
Sonal Garg <sgarg24@sapient.com>
2017-01-02 12:44:14 +0530

    Green: added minimal logic for single item in cart

--- src/main/java/com/practise/tdd/Checkout.java
+++ src/main/java/com/practise/tdd/Checkout.java
@@ -1,8 +1,13 @@
 package com.practise.tdd;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Checkout {
 
 	public Integer total(String cart) {
+		if(StringUtils.equals(cart, "A")) {
+			return new Integer(50);
+		}
 		return 0;
 	}
 	
