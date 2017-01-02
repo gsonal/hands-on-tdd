8360ef6287894001cea90625d36b5af6b3938806
Sonal Garg <sgarg24@sapient.com>
2017-01-02 18:43:50 +0530

    Green: Added logic to apply promotions

--- src/main/java/com/practise/tdd/Checkout.java
+++ src/main/java/com/practise/tdd/Checkout.java
@@ -1,6 +1,10 @@
 package com.practise.tdd;
 
+import static java.lang.String.valueOf;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.practise.tdd.dao.ProductPriceDao;
 
@@ -15,14 +19,37 @@ public class Checkout {
 	
 	public Integer total(String cart) {
 		Integer totalCost = 0;
+		Map<String, Integer> productCountInCart = new HashMap<>();
 
 		for(Character item : cart.toCharArray()) {
-			totalCost += productPriceDao.getPriceOf(String.valueOf(item));
+			String itemCode = valueOf(item);
+			totalCost += productPriceDao.getPriceOf(itemCode);
+			productCountInCart.put(itemCode, productCountInCart.getOrDefault(itemCode, 0)+1);
+		}
+		
+		totalCost = adjustForPromotions(productCountInCart, totalCost);
+		
+		return totalCost;
+	}
+
+	private Integer adjustForPromotions(Map<String, Integer> productCountInCart, Integer totalCost) {
+		for(Promotions promotion : ongoingPromotions) {
+			String product = promotion.getProductSku();
+			Integer countEligibleForDiscount = promotion.getQuantity();
+			Integer discountedPrice = promotion.getDiscountedPrice();
+			
+			if(productEligibleForPromotion(productCountInCart, promotion)) {
+				Integer priceForPurchasedQnty = countEligibleForDiscount * productPriceDao.getPriceOf(product);
+				totalCost -= (priceForPurchasedQnty - discountedPrice);
+			}
 		}
 		
 		return totalCost;
 	}
 
-	
+	private boolean productEligibleForPromotion(Map<String, Integer> productCountInCart, Promotions promotion) {
+		String product = promotion.getProductSku();
+		return (productCountInCart.containsKey(product) && promotion.getQuantity() == productCountInCart.get(product));
+	}
 	
 }
