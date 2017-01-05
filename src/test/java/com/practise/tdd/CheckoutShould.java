da495939ef942a29b132ce2b069e985a87e8c203
Sonal Garg <sgarg24@sapient.com>
2017-01-05 14:14:12 +0530

    Updated test class to Parameterized Junit

--- src/test/java/com/practise/tdd/CheckoutShould.java
+++ src/test/java/com/practise/tdd/CheckoutShould.java
@@ -2,18 +2,31 @@ package com.practise.tdd;
 
 import static java.lang.String.valueOf;
 import static org.junit.Assert.assertEquals;
+import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value = Parameterized.class)
 public class CheckoutShould {
 
 	Checkout checkout;
 	List<Promotions> runningPromotions;
+	String itemsInCart;
+	Integer paymentDue;
+	
+	public CheckoutShould(String itemsInCart, Integer paymentDue) {
+		this.itemsInCart = itemsInCart;
+		this.paymentDue = paymentDue;
+	}
 	
 	@Before
 	public void doSetup() {
@@ -21,49 +34,24 @@ public class CheckoutShould {
 		checkout = new Checkout(runningPromotions);
 	}
 	
-	@Test
-	public void return_0_for_empty_cart() {
-		assertEquals(new Integer(0), checkout.total(scanEachItem("")));
-	}
-
-	@Test
-	public void return_50_for_cart_having_A() {
-		assertEquals(new Integer(50), checkout.total(scanEachItem("A")));
-	}
-	
-	@Test
-	public void return_100_for_cart_having_AA() {
-		assertEquals(new Integer(100), checkout.total(scanEachItem("AA")));
-	}
-	
-	@Test
-	public void return_130_for_cart_having_AAB() {
-		assertEquals(new Integer(130), checkout.total(scanEachItem("AAB")));
+	@Parameters(name = "print_{1}_for_{0}_number() = {1}")
+	public static Collection<Object[]> data() {
+		return asList(new Object[][] {
+			{"", 0},
+			{"A", 50},
+			{"AA", 100},
+			{"AAB", 130},
+			{"AAA", 130},
+			{"AAAAAB", 260},
+			{"AAAAAA", 260},
+			{"AAAAAABD", 305},
+			{"AAABBD", 190}
+		});
 	}
 	
 	@Test
-	public void return_130_for_cart_having_AAA() {
-		assertEquals(new Integer(130), checkout.total(scanEachItem("AAA")));
-	}
-	
-	@Test
-	public void return_260_for_cart_having_AAAAAB() {
-		assertEquals(new Integer(260), checkout.total(scanEachItem("AAAAAB")));
-	}
-	
-	@Test
-	public void return_260_for_cart_having_AAAAAA() {
-		assertEquals(new Integer(260), checkout.total(scanEachItem("AAAAAA")));
-	}
-	
-	@Test
-	public void return_305_for_cart_having_AAAAAA() {
-		assertEquals(new Integer(305), checkout.total(scanEachItem("AAAAAABD")));
-	}
-	
-	@Test
-	public void return_190_for_cart_having_AAABBD() {
-		assertEquals(new Integer(190), checkout.total(scanEachItem("AAABBD")));
+	public void return_total_cost_of_purchased_items() {
+		assertEquals(paymentDue, checkout.total(scanEachItem(itemsInCart)));
 	}
 	
 	private List<Promotions> getAvailablePromotions() {
@@ -84,8 +72,7 @@ public class CheckoutShould {
 			for(Character item : cartItems.toCharArray()) {
 				checkout.scan(valueOf(item), cart);
 			}
-		}
-		
+		}		
 		return cart;
 	}
 }
